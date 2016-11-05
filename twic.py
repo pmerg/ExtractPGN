aebf53241ac4a5c823e9553f8a86a45fc689de36
Ted Wong <WongMingYin@gmail.com>
2016-11-05 12:54:03 +1000

    Python script for downloading TWIC games

---
+++ twic.py
@@ -0,0 +1,29 @@
+#
+# Script for loading and merging TWIC chess games. This is a free and efficient way to build a megadatabase for chess.
+#
+
+from urllib import urlopen
+from zipfile import ZipFile
+from StringIO import StringIO
+
+def downloadPGN(issue):
+    issue = 'http://www.theweekinchess.com/zips/twic' + str(issue) + 'g.zip'
+    
+    try:
+        zipFile = ZipFile(StringIO(urlopen(issue).read()))
+
+        # Eg: twic1147.pgn
+        files = [x for x in zipFile.namelist() if x.endswith('.pgn')] 
+        
+        return ''.join(zipFile.open(files[0]).readlines())
+    except Exception:
+        print ('Failed to download: ' + str(issue))
+        return ''
+
+def downloadPGNs(start, end):
+    games = ''    
+    for i in range(start, end):
+        games = games + downloadPGN(i)
+    return games
+        
+print (downloadPGNs(1145, 1147))
